6c03a54f9bb85f0982183a08eebfe129e1462985
ekeshfsociety <ekesh.taneja1@learner.manipal.edu>
2020-09-14 13:59:41 +0530

    September Long 2020

---
+++ Codechef Long(September 2020)/ADAMATRIX.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n ;
+
+        int arr[n][n];
+        for(int i=0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<n ; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+
+        int count {} ;
+
+        for(int j=n-1 ; j>0 ; j--)
+        {
+            if(arr[0][j] != j+1 )
+            {
+                count++ ;
+                for(int x=0 ; x<=j ; x++)
+                {
+                    for(int y=x+1 ; y<=j ; y++)
+                    {
+                        swap(arr[x][y] , arr[y][x]) ;
+                    }
+                }
+            }
+        }
+
+        cout << count << endl ;
+
+    }
+
+    return 0;
+}
